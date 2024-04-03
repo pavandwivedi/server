70fc69b1346c8dd50f1bb255c4b947e1f33def6e
pavandwivedi <pavandwivedi35@gmail.com>
2024-04-03 17:52:44 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import morgan from 'morgan'
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ||3000;
+const port = process.env.PORT ||11000;
 
 app.use(express.json());
 app.use(cors());
